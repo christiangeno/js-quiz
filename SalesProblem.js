ed4847c9c317f6f13525f3a6e535d06b2e9fbe1c
christiangeno <61463952+christiangeno@users.noreply.github.com>
2023-07-04 09:14:41 +0800

    initial commit

---
+++ SalesProblem.js
@@ -0,0 +1,24 @@
+const productProfitArray = [
+    { name: "Product A", profit: 5 },
+    { name: "Product B", profit: -70 },
+    { name: "Product C", profit: 93 },
+    { name: "Product D", profit: -5 },
+    { name: "Product E", profit: 88 },
+    { name: "Product F", profit: 29 },
+    { name: "Product G", profit: 93.1 }, 
+];
+
+    const topProduct=()=>{
+        return productProfitArray.reduce((prev,curr)=> curr.profit > prev.profit ? curr : prev)
+    };
+    console.log(topProduct());
+
+    const bottomProduct=()=>{
+        return productProfitArray.reduce((prev,curr)=> curr.profit < prev.profit ? curr : prev)
+    };
+    console.log(bottomProduct());
+
+    const zeroProfitProduct=()=>{
+        return productProfitArray.reduce((prev,curr)=> Math.abs(curr.profit) < Math.abs(prev.profit) ? curr : prev)
+    };
+    console.log(zeroProfitProduct());
